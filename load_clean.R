2005e6505670b4d9a0de437b8f89a5c2660bee47
unknown <jeroenwasser@gmail.com>
2022-10-31 10:56:06 +0100

    changed directory to be dynamic in path

--- load_clean.R
+++ load_clean.R
@@ -1,7 +1,7 @@
 library("readxl")
 
 
-my_data <- read_excel("C:/Users/Jeroen/Documents/School/Master_Year1/Social Network Analysis/Project/Data_Extract_From_Global_Bilateral_Migration.xlsx")
+my_data <- read_excel("Data_Extract_From_Global_Bilateral_Migration.xlsx")
 
 drop <- c("Year Code", "Country Origin Code", "Migration by Gender Name", "Migration by Gender Code")
 migration_data = my_data[,!(names(my_data) %in% drop)]
