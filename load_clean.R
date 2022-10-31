e66b6d8d83f89c30cf9b0c098ebf3b738388b7ec
Maud Groen <maudgroen2000@gmail.com>
2022-10-31 15:35:28 +0100

    Change data labels

--- load_clean.R
+++ load_clean.R
@@ -6,7 +6,7 @@ my_data <- read_excel("Data_Extract_From_Global_Bilateral_Migration.xlsx")
 
 drop <- c("Year Code", "Country Origin Code", "Migration by Gender Name", "Migration by Gender Code")
 migration_data = my_data[,!(names(my_data) %in% drop)]
-#df <- setNames(migration_data, c("Year", "Country","Belarus", "Czech Republic", "Germany", "Hungary", "Macedonia, FYR", "Russian Federation", "Ukraine", "Austria", "Belgium", "Bulgaria", "Croatia", "Denmark","Estonia", "France", "Finland", "Georgia", "Greece", "Iceland", "Ireland", "Italy", "Liechtenstein", "Lithuani", "Slovenia", "Spain", "Sweden", "Switzerland", "United Kingdom", "Albania", "Bosnia and Herzegovina", "Cyprus", "Latvia", "Monaco", "Slovak Republic"))
+migration_data <- setNames(migration_data, c("Year", "Country", "Belarus", "Czech Republic", "Germany", "Hungary", "Macedonia, FYR", "Russian Federation", "Ukraine", "Austria", "Belgium", "Bulgaria", "Croatia", "Denmark", "Estonia", "France", "Finland", "Georgia", "Greece", "Iceland", "Ireland", "Italy", "Liechtenstein", "Lithuania", "Luxembourg", "Netherlands", "Norway", "Poland", "Portugal", "Romania", "Slovenia", "Spain", "Sweden", "Switzerland", "United Kingdom", "Albania", "Bosnia and Herzegovina", "Cyprus", "Latvia", "Monaco", "Slovak Republic"))
 
 write_xlsx(migration_data, "migration_data.xlsx")
 
@@ -15,8 +15,3 @@ migration_1970 <- migration_data %>% filter(Year == 1970)
 migration_1980 <- migration_data %>% filter(Year == 1980)
 migration_1990 <- migration_data %>% filter(Year == 1990)
 migration_2000 <- migration_data %>% filter(Year == 2000)
-
-test <- igraph::graph_from_data_frame(migration_1960, directed = TRUE, vertices = NULL)
-
-
-igraph::tkplot(test)
