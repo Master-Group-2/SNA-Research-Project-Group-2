6fb9573c3a24d2186423ac7147af1c4de04dade6
HarmBooy <55991709+HarmBooy@users.noreply.github.com>
2022-10-31 10:59:22 +0100

    column name change

--- load_clean.R
+++ load_clean.R
@@ -5,5 +5,5 @@ my_data <- read_excel("Data_Extract_From_Global_Bilateral_Migration.xlsx")
 
 drop <- c("Year Code", "Country Origin Code", "Migration by Gender Name", "Migration by Gender Code")
 migration_data = my_data[,!(names(my_data) %in% drop)]
-
+df <- setNames(df, c("Belarus", "Czech Republic", "Germany", "Hungary", "Macedonia, FYR", "Russian Federation", "Ukraine", "Austria", "Belgium", "Bulgaria", "Croatia", "Denmark","Estonia", "France", "Finland", "Georgia", "Greece", "Iceland", "Ireland", "Italy", "Liechtenstein", "Lithuani", "Slovenia", "Spain", "Sweden", "Switzerland", "United Kingdom", "Albania", "Bosnia and Herzegovina", "Cyprus", "Latvia", "Monaco", "Slovak Republic"))
 migration_data
