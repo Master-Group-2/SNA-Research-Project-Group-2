78ab2be3b716509033cc35cf2f246722b4269212
unknown <jeroenwasser@gmail.com>
2022-10-31 12:30:15 +0100

    script that changes dataframe to igraph

--- dataframe_to_network.R
+++ dataframe_to_network.R
@@ -2,5 +2,6 @@ library("readxl")
 
 my_data <- read_excel("migration_data.xlsx")
 
-test <- graph_from_data_frame(my_data, directed = TRUE, vertices = NULL)
+test <- igraph::graph_from_data_frame(my_data, directed = TRUE, vertices = NULL)
 
+plot(test)
