c41eca4c341078131aa31c62a0379e64ddc058d1
unknown <jeroenwasser@gmail.com>
2022-11-10 14:57:05 +0100

    Finished cleaning and converting datasets

--- load_clean.R
+++ load_clean.R
@@ -26,6 +26,58 @@ migration_data_list[[5]] <- migration_2000
 
 migration_networks <- list()
 
+add_region_attribute <- function(graph){
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 1, 'South')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 2, 'West')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 3, 'East')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 4, 'West')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 5, 'South')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 6, 'East')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 7, 'South')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 8, 'South')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 9, 'East')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 10, 'North')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 11, 'North')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 12, 'North')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 13, 'West')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 14, 'East')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 15, 'West')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 16, 'South')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 17, 'East')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 18, 'North')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 19, 'North')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 20, 'South')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 21, 'North')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 22, 'West')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 23, 'North')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 24, 'West')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 25, 'South')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 26, 'South')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 27, 'West')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 28, 'North')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 29, 'East')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 30, 'South')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 31, 'East')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 32, 'East')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 33, 'East')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 34, 'South')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 35, 'South')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 36, 'North')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 37, 'West')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 38, 'East')
+  graph <- igraph::set.vertex.attribute(graph, 'region', index = 39, 'North')
+  
+  colrs <- c("South"="yellow", "West"="green", "East"="red", "North"="blue")
+  igraph::V(graph)$color <- colrs[igraph::V(graph)$region]
+  return(graph)
+}
+
+# First creating the function to sclae
+fun_range <- function(x) {                              # Create user-defined function
+  (x - min(x)) / (max(x)-min(x))
+}
+
+
 for(i in 1:length(migration_data_list))
 {
   migration_data_list[[i]]$Year <- NULL
@@ -42,111 +94,34 @@ for(i in 1:length(migration_data_list))
   migration_networks[[i]] <- igraph::graph_from_adjacency_matrix(migration_matrix,
                                               mode = c('directed'),
                                               weighted = TRUE)
+  migration_networks[[i]] <- add_region_attribute(migration_networks[[i]])
+  
+  weightscaled <- fun_range(x = igraph::edge.attributes(migration_networks[[i]])$weight)
+  migration_networks[[i]] <- igraph::set.edge.attribute(migration_networks[[i]], "weight.scaled", index = igraph::E(migration_networks[[i]]), weightscaled)
+  
 }
 
 migration_data_list
 migration_networks
 
+#test <- igraph::delete.edges(test, which(igraph::E(test)$weight<10000))
+#test <- igraph::simplify(test)
 
 
-summary(test)
-igraph::edge.attributes(test)
-test <- igraph::delete.edges(test, which(igraph::E(test)$weight<10000))
-test <- igraph::simplify(test)
-test
-igraph::tkplot(test)
-
-# Adding an extra edge attribute with scaled weights
-# First creating the function to sclae
-fun_range <- function(x) {                              # Create user-defined function
-  (x - min(x)) / (max(x)-min(x))
-}
-
-# Then running the function and adding it to our graph 
-weightscaled <- fun_range(x = igraph::edge.attributes(test)$weight)
-test <- igraph::set.edge.attribute(test, "weight.scaled", index = igraph::E(test), weightscaled)
-
-test <- igraph::set.vertex.attribute(test, 'region', index = 1, 'South')
-test <- igraph::set.vertex.attribute(test, 'region', index = 2, 'West')
-test <- igraph::set.vertex.attribute(test, 'region', index = 3, 'East')
-test <- igraph::set.vertex.attribute(test, 'region', index = 4, 'West')
-test <- igraph::set.vertex.attribute(test, 'region', index = 5, 'South')
-test <- igraph::set.vertex.attribute(test, 'region', index = 6, 'East')
-test <- igraph::set.vertex.attribute(test, 'region', index = 7, 'South')
-test <- igraph::set.vertex.attribute(test, 'region', index = 8, 'South')
-test <- igraph::set.vertex.attribute(test, 'region', index = 9, 'East')
-test <- igraph::set.vertex.attribute(test, 'region', index = 10, 'North')
-test <- igraph::set.vertex.attribute(test, 'region', index = 11, 'North')
-test <- igraph::set.vertex.attribute(test, 'region', index = 12, 'North')
-test <- igraph::set.vertex.attribute(test, 'region', index = 13, 'West')
-test <- igraph::set.vertex.attribute(test, 'region', index = 14, 'East')
-test <- igraph::set.vertex.attribute(test, 'region', index = 15, 'West')
-test <- igraph::set.vertex.attribute(test, 'region', index = 16, 'South')
-test <- igraph::set.vertex.attribute(test, 'region', index = 17, 'East')
-test <- igraph::set.vertex.attribute(test, 'region', index = 18, 'North')
-test <- igraph::set.vertex.attribute(test, 'region', index = 19, 'North')
-test <- igraph::set.vertex.attribute(test, 'region', index = 20, 'South')
-test <- igraph::set.vertex.attribute(test, 'region', index = 21, 'North')
-test <- igraph::set.vertex.attribute(test, 'region', index = 22, 'West')
-test <- igraph::set.vertex.attribute(test, 'region', index = 23, 'North')
-test <- igraph::set.vertex.attribute(test, 'region', index = 24, 'West')
-test <- igraph::set.vertex.attribute(test, 'region', index = 25, 'South')
-test <- igraph::set.vertex.attribute(test, 'region', index = 26, 'South')
-test <- igraph::set.vertex.attribute(test, 'region', index = 27, 'West')
-test <- igraph::set.vertex.attribute(test, 'region', index = 28, 'North')
-test <- igraph::set.vertex.attribute(test, 'region', index = 29, 'East')
-test <- igraph::set.vertex.attribute(test, 'region', index = 30, 'South')
-test <- igraph::set.vertex.attribute(test, 'region', index = 31, 'East')
-test <- igraph::set.vertex.attribute(test, 'region', index = 32, 'East')
-test <- igraph::set.vertex.attribute(test, 'region', index = 33, 'East')
-test <- igraph::set.vertex.attribute(test, 'region', index = 34, 'South')
-test <- igraph::set.vertex.attribute(test, 'region', index = 35, 'South')
-test <- igraph::set.vertex.attribute(test, 'region', index = 36, 'North')
-test <- igraph::set.vertex.attribute(test, 'region', index = 37, 'West')
-test <- igraph::set.vertex.attribute(test, 'region', index = 38, 'East')
-test <- igraph::set.vertex.attribute(test, 'region', index = 39, 'North')
-
-colrs <- c("South"="yellow", "West"="green", "East"="red", "North"="blue")
-igraph::V(test)$color <- colrs[igraph::V(test)$region]
-
-igraph::get.vertex.attribute(test, 'color')
-
-eastWest <- igraph::delete.vertices(test, which(igraph::V(test)$region=='North'))
-eastWest2 <- igraph::delete.vertices(eastWest, which(igraph::V(eastWest)$region=='South'))
-
-eastWest
-eastWest2
+#eastWest <- igraph::delete.vertices(test, which(igraph::V(test)$region=='North'))
+#eastWest2 <- igraph::delete.vertices(eastWest, which(igraph::V(eastWest)$region=='South'))
 
 # Plotting the network 
-plot(test,
-     main= "Migrations between EU countries 1960",
-     edge.width = igraph::E(test)$weight.scaled*20,
-     edge.arrow.size = 0.1,
-     curved = TRUE,
-     vertex.size = 20,
-     layout = igraph::layout_on_sphere
-)
-
-
-plot(test,
-     vertex.shape="circle",
-     layout = igraph::layout_on_grid,
-     main= "Migrations between EU countries 1960",
-     edge.width = igraph::E(test)$weight.scaled*15,
-     edge.arrow.size = 0.1,
-     vertex.size = 10,
-)
-
-plot(test,
+plot(migration_networks[[1]],
      vertex.shape="circle",
      layout = igraph::layout_randomly,
      main= "Migrations between EU countries 1960",
-     edge.width = igraph::E(test)$weight.scaled*15,
+     edge.width = igraph::E(migration_networks[[1]])$weight.scaled*15,
      edge.arrow.size = 0.1,
-     vertex.color = igraph::V(test)$color,
+     vertex.color = igraph::V(migration_networks[[1]])$color,
      vertex.size = 10,
      vertex.label.color = 'black',
 )
 
-
+migration_networks[[1]]
 
