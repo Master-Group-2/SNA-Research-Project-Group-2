482aab8243badfb57df328fb008fb0b386ae8d0e
HarmBooy <55991709+HarmBooy@users.noreply.github.com>
2022-11-15 14:05:13 +0100

    names with capital letter

--- characterintegration.R
+++ characterintegration.R
@@ -8,7 +8,7 @@ my_data <- read_excel("5.1/Social Network Analysis for Data Scientists/Groupproj
 drop <- c("Time Code", "Country Code")
 info_data = my_data[,!(names(my_data) %in% drop)]
 colnames(info_data)
-info_data_2 <- setNames(info_data, c("Year", "Country", "Airtransport", "energy", "Cereal", "Electricpower", "Employers", "telephonesubscriptions", "GDP", "Internet", "Lifeexpectancy", "techexports", "techmanufacturing", "Mobilesubscriptions", "Mortalityfemale", "Mortalitymale", "drinkingwater", "Journalarticles", "Internetservers", "Survival65female", "Survival65male", "Technicians"))
+info_data_2 <- setNames(info_data, c("Year", "Country", "Airtransport", "Energy", "Cereal", "Electricpower", "Employers", "Telephonesubscriptions", "GDP", "Internet", "Lifeexpectancy", "Techexports", "Techmanufacturing", "Mobilesubscriptions", "Mortalityfemale", "Mortalitymale", "Drinkingwater", "Journalarticles", "Internetservers", "Survival65female", "Survival65male", "Technicians"))
 
 
 write_xlsx(info_data_2, "info_data.xlsx")
