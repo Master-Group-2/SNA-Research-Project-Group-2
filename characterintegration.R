53f2a0f521776261ba0d6e48b73eac46f62d2c0d
HarmBooy <55991709+HarmBooy@users.noreply.github.com>
2022-11-15 14:02:55 +0100

    column names changed

--- characterintegration.R
+++ characterintegration.R
@@ -3,12 +3,12 @@ library("writexl")
 library("dplyr")
 
 
-my_data <- read_excel("attributes_data.xlsx")
+my_data <- read_excel("5.1/Social Network Analysis for Data Scientists/Groupproject/SNA-Research-Project-Group-2/attributes_data.xlsx")
 
 drop <- c("Time Code", "Country Code")
 info_data = my_data[,!(names(my_data) %in% drop)]
 colnames(info_data)
-info_data_2 <- setNames(info_data, c("Year", "Country", "Air transport departures worldwide", "Alternative and nuclear energy (% of total energy use)", "Cereal production (metric tons)", "Electric power consumption (kWh per capita)", "Employers, total (% of total employment)", "Fixed telephone subscriptions", "GDP growth (annual %)", "Individuals using the Internet (% of population)", "Life expectancy at birth, total (years)", "Medium and high-tech exports (% manufactured exports)", "Medium and high-tech manufacturing value added (% manufacturing value added)", "Mobile cellular subscriptions", "Mortality rate, adult, female (per 1,000 female adults)", "Mortality rate, adult, male (per 1,000 male adults)", "People using safely managed drinking water services (% of population)", "Scientific and technical journal articles", "Secure Internet servers", "Survival to age 65, female (% of cohort)", "Survival to age 65, male (% of cohort)", "Technicians in R&D (per million people)"))
+info_data_2 <- setNames(info_data, c("Year", "Country", "Airtransport", "energy", "Cereal", "Electricpower", "Employers", "telephonesubscriptions", "GDP", "Internet", "Lifeexpectancy", "techexports", "techmanufacturing", "Mobilesubscriptions", "Mortalityfemale", "Mortalitymale", "drinkingwater", "Journalarticles", "Internetservers", "Survival65female", "Survival65male", "Technicians"))
 
 
 write_xlsx(info_data_2, "info_data.xlsx")
