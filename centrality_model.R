8736c72d57f033a4c6eac07c9fece560a83a9494
Koen van Hugten <koenvanhugten@gmail.com>
2022-11-26 11:00:26 +0100

    minor updates centrality model

--- centrality_model.R
+++ centrality_model.R
@@ -1,3 +1,5 @@
+library(gridExtra)
+
 ## Load cleaned data
 load('migration_networks.RData')
 
@@ -37,9 +39,55 @@ plot(migration_networks[[1]],
      vertex.shape = 'circle',
      vertex.color = color, 
      vertex.size = sqrt(central_data[['Centrality_1960']]),
-#     edge.width = igraph::E(migration_networks[[1]])$weight.scaled*15,
+     edge.width = igraph::E(migration_networks[[1]])$weight.scaled*15,
      edge.arrow.size = 0.2,
-     vertex.label.color = 'black')
-#     layout = igraph::layout.reingold.tilford(migration_networks[[1]]))
+     vertex.label.color = 'black',
+     layout = igraph::layout_in_circle)
 
+
+plot_centralities <- function(networks, years){
+  central_data = centrality_analysis(networks, years)
+  plots = list()
+  
+  for (year_idx in 1:length(years)){
+    color <- rep('red', snafun::count_vertices(networks[[year_idx]]))
+    central_countries <- which(central_data[[year_idx + 1]] > 50)
+    color[central_countries] <- 'green'
+    
+    plots[[year_idx]] <- recordPlot(plot(networks[[year_idx]],
+                                         vertex.shape = 'circle',
+                                         vertex.color = color,
+                                         vertex.size = sqrt(central_data[[year_idx + 1]]),
+                                         edge.width = igraph::E(networks[[year_idx]])$weight.scaled*15,
+                                         edge.arrow.size = 0.2,
+                                         vertex.label.color = 'black',
+                                         main = sprintf('Centrality per country in %s', years[year_idx]),
+                                         layout = igraph::layout_in_circle))
+  }
+  #grid.arrange(grobs = plots, nrow = length(years)%/%2 + 1)
+  plots
+}
+
+test <- plot_centralities(migration_networks, years)
+
+grid.arrange(test)
+class(test[[1]])
+
+## Plot layouts
+# layout_on_sphere (2)
+# layout_nicely
+# layout_as_tree
+# layout.fruchterman.reingold
+# layout_in_circle (1)
+# layout_as_star
+# layout_on_grid (2)
+# layout_randomly
+# layout_with_dh 
+# layout_with_fr
+# layout_with_gem
+# layout_with_graphopt
+# layout_with_kk (3)
+# layout_with_lgl (3)
+# layout_with_mds
+# layout_with_sugiyama
 
